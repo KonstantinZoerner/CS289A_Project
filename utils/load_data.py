b409ac4db7276b0302cf7600f054e3ab2c96f4fa
Konstantin Georg Zörner <konstantin.zoerner@tuhh.de>
2025-05-05 17:11:58 -0700

    Add Data class to load data

---
+++ utils/load_data.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class Data:
+
+    def __init__(self, dataset="cancer"):
+        # find path to dataset
+        if dataset == "cancer":
+            path = "datasets/breast_cancer/data.csv"
+        elif dataset == "diabetes":
+            path = "datasets/diabetes/diabetes_binary_health_indicators_BRFSS2015.csv"
+        else:
+            raise NotImplementedError("This dataset does not exist")
+
+        # load data
+        data = np.genfromtxt(path, delimiter=',', dtype=None)
+        
+        # split labels and features
+        if dataset == "cancer":
+            self.labels = data[:, [1]]
+            self.features = data[:, 2:]
+        else:
+            self.labels = data[:, [0]]
+            self.features = data[:, 1:]
+
+
+if __name__ == "__main__":
+    data = Data(dataset = "diabetes")
