8c1e2dea1fa6f6cd8f0267ac7c89c2a53fc0b018
TillM2T <till@m2-tech.io>
2025-05-06 19:40:39 -0700

    Initial commit

---
+++ models/logistic_regression.py
@@ -0,0 +1,10 @@
+from sklearn.linear_model import LogisticRegression
+
+
+def LogisticRegression(LogisticRegression):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def model_size(self):
+        #TODO
+        pass
