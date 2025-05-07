d45d7bc4685f5a6ba78d0f23d9fee6227322343a
TillM2T <till@m2-tech.io>
2025-05-06 17:09:33 -0700

    change input formating to (n,)

--- src/losses.py
+++ src/losses.py
@@ -8,6 +8,6 @@ class One_Zero_Loss(Loss):
     def __call__(self, y, y_hat):
         # print(y.shape, y_hat.shape)
         # print(y, y_hat)
-        y = y.reshape((-1,1))
-        y_hat = y_hat.reshape((-1,1))
+        y = y.reshape((-1,))
+        y_hat = y_hat.reshape((-1,))
         return 1 - np.sum(y == y_hat) / y.size
