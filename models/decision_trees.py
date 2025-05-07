b4766ea3f3ca3376acb14c2972f9bd146fff7a4f
TillM2T <till@m2-tech.io>
2025-05-06 19:20:12 -0700

    Implement random forest

--- models/decision_trees.py
+++ models/decision_trees.py
@@ -1,4 +1,5 @@
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 class DecicisonTree(DecisionTreeClassifier):
     def __init__(self, **kwargs):
@@ -7,3 +8,11 @@ class DecicisonTree(DecisionTreeClassifier):
     def model_size(self):
         #TODO what about leaf nodes
         return self.tree_.node_count * 2
+    
+class RandomForest(RandomForestClassifier):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def model_size(self):
+        #TODO what about leaf nodes
+        return self.tree_.node_count * 2 * self.n_estimators
