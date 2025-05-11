75cf5aceb26da7813b0c6833da00082018ecb5ae
TillM2T <till@m2-tech.io>
2025-05-10 17:43:33 -0700

    neural network tuning

--- test_model.py
+++ test_model.py
@@ -16,13 +16,13 @@ import matplotlib.pyplot as plt
 rng = np.random.default_rng(1)
 
 # set model
-model = nn.NeuralNetwork()
+model = nn.NeuralNetwork(verbose=True)
 
 # datasets = ["cancer", "diabetes"]
 # runs = [100, 1]
-datasets = ["cancer"]
+datasets = ["diabetes"]
 runs = [1]
-ratio = 1
+ratio = 0.5
 loss = One_Zero_Loss()
 
 for i, dataset in enumerate(datasets):
@@ -34,7 +34,7 @@ for i, dataset in enumerate(datasets):
     for run in tqdm(range(runs[i])):
         data.split_by_ratio(0.8 * ratio, 0.2, 0.0, rng)
         t_0 = time.perf_counter()
-        model.fit(data.train_features, data.train_labels, epochs=50, model=None,X_val=data.val_features,y_val=data.val_labels)
+        model.fit(data.train_features, data.train_labels, epochs=10, model=None,X_val=data.val_features,y_val=data.val_labels)
         t_1 = time.perf_counter()
         y_pred = model.predict(data.val_features)  
         t_2 = time.perf_counter()
